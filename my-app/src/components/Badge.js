aee68abcf7331caa3f4f8c5a3f7d8734161b5887
Tanvanat <tanvanat112@gmail.com>
2025-09-24 14:10:33 +0700

    delete unuse files

--- my-app/src/components/Badge.js
+++
@@ -1,9 +0,0 @@
-export default function Badge({ children, className = "" }) {
-  return (
-    <span
-      className={`inline-flex items-center rounded-full border px-2.5 py-1 text-xs ${className}`}
-    >
-      {children}
-    </span>
-  );
-}
